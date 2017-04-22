4d5592ca454dfc1e144fd57ca26a87eabf080ff7
Hack Reactor Student <student@hackreactor.com>
2017-04-21 20:44:41 -0700

    Completes sql schema file

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,35 @@
-CREATE DATABASE chat;
+CREATE DATABASE myChat1;
 
-USE chat;
+USE myChat1;
+
+CREATE TABLE users (
+	id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL
+ /* Describe your table here.*/
+);
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+	id INTEGER PRIMARY KEY,
+  message TEXT NOT NULL,
+  user INTEGER,
+  FOREIGN KEY(user) REFERENCES users(id)
 );
+
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  room TEXT NOT NULL
+);
+
+CREATE TABLE connection (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  message_id INTEGER,
+  FOREIGN KEY(message_id) REFERENCES messages(id),
+  room_id INTEGER,
+  FOREIGN KEY(room_id) REFERENCES rooms(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
